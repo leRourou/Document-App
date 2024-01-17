e6cd1a5d9fc2b3a2d4d2600172c62342ce7abfd0
Adrien CHENU <chenuad@d250-m01.iut-acy.local>
2024-01-17 14:01:16 +0100

    Done working with QLPreview

--- Document App/DocumentTableViewController.swift
+++ Document App/DocumentTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import QuickLook
 
 extension Int{
     func formattedSize() -> String{
@@ -14,6 +15,7 @@ extension Int{
 }
 
 class DocumentTableViewController: UITableViewController {
+    let quickLookController = QLPreviewController()
 
     struct DocumentFile{
         var title: String
@@ -21,41 +23,19 @@ class DocumentTableViewController: UITableViewController {
         var imageName: String? = nil
         var url: URL
         var type: String
-        
-        static var documentList: [DocumentFile] = [
-            DocumentFile(title: "Document 1", size: 100, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 2", size: 200, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 3", size: 300, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 4", size: 400, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 5", size: 500, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 6", size: 600, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 7", size: 700, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 8", size: 800, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 9", size: 900, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-            DocumentFile(title: "Document 10", size: 1000, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        ]
     }
 
     func listFileInBundle() -> [DocumentFile] {
-        // On instancie un objet de type FileManager permettant d'effectuer des actions sur le sytème de fichier
         let fm = FileManager.default
-        // On obtient le path des "resources"
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
-        // On Initialise la liste de documents
         var documentListBundle = [DocumentFile]()
     
-        // Pour chaque fichier trouvé dans "items"
         for item in items {
-            // On prends les fichers qui n'ont pas le suffixe DS_Store et on le suffixe .jpg
             if !item.hasSuffix("DS_Store") && item.hasSuffix(".jpg") {
-                // On instancie un objet de type URL qui sera le path de notre fichier (son dossier parent / son nom)
                 let currentUrl = URL(fileURLWithPath: path + "/" + item)
-                // On récupère des informations importantes sur le fichier avec la méthode "resourceValues" de la classe URL
                 let resourcesValues = try! currentUrl.resourceValues(forKeys: [.contentTypeKey, .nameKey, .fileSizeKey])
-                   
-                // On ajoute à la liste initialement créé un objet de type DocumentFile avec comme propriétés les informations récupérés précedemment.
                 documentListBundle.append(DocumentFile(
                     title: resourcesValues.name!,
                     size: resourcesValues.fileSize ?? 0,
@@ -70,18 +50,9 @@ class DocumentTableViewController: UITableViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
+        quickLookController.dataSource = self;
     }
 
-    // MARK: - Table view data source
-
-    // Indique au Controller combien de sections il doit afficher
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -89,76 +60,34 @@ class DocumentTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listFileInBundle().count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+        let document = listFileInBundle()[indexPath.row]
+        cell.textLabel?.text = document.title
+        cell.detailTextLabel?.text = document.size.formattedSize()
 
-        var content = cell.defaultContentConfiguration()
-        content.text = listFileInBundle()[indexPath.row].title
-        content.secondaryText = listFileInBundle()[indexPath.row].size.formattedSize()
-        cell.contentConfiguration = content
-        
         return cell
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // 1. Récuperer l'index de la ligne 
-        var index = self.tableView.indexPathForSelectedRow
-        // 2. Récuperer le document correspondant à l'index
-        if let index {
-            var doc = listFileInBundle()[index.row]
-            let destView = segue.destination as! DocumentViewController
-            destView.imageName = doc.imageName
-        }
-        // 3. Cibler l'instance de DocumentViewController via le segue.destination
-        // 4. Caster le segue.destination en DocumentViewController
-        // 5. Remplir la variable imageName de l'instance de DocumentViewController avec le nom de l'image du document
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let file = listFileInBundle()[indexPath.row].url
+        self.instantiateQLPreviewController(withUrl: file)
+    }
+    
+    func instantiateQLPreviewController(withUrl url: URL) {
+        quickLookController.currentPreviewItemIndex = 0
+        present(quickLookController, animated: true, completion: nil)
+    }
+}
+
+extension DocumentTableViewController: QLPreviewControllerDataSource {
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        let file = listFileInBundle()[index]
+        return file.url as QLPreviewItem
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
